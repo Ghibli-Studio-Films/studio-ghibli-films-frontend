6e3141fecb08624cbc411696b7004b0a1de86c68
leogm360 <leogm360@gmail.com>
2022-08-16 22:55:54 -0300

    Initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  port: 3001,
+  viewportWidth: 1440,
+  viewportHeight: 900,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
