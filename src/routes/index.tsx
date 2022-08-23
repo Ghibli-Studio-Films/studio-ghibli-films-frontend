7c641418ac50cf4a10bf509d805bc58f9455cee4
leogm360 <leogm360@gmail.com>
2022-08-23 15:27:40 -0300

    ✨ feat: implement routing

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+export const Navigation = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<></>} />
+    </Routes>
+  );
+};
